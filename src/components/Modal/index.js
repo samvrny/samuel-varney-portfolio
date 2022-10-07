7d1a6ff1c80aedb474d1d80fdf12a130515f0952
samvrny <samvrny@gmail.com>
2022-10-06 21:49:40 -0500

    finished with the portfolio, for now

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,13 +1,17 @@
 function Modal({ currentProject, onClose }) {
 
-    const { name, description, picture } = currentProject
+    const { name, description, picture, github, deployed } = currentProject
 
     return (
         <div className='modal-holder'>
             <div className="modalBackdrop">
                 <div className="modal-container">
                     <h3 className="modal-title">{name}</h3>
-                    <img className='modal-picture' src={picture} alt={name} />
+                    <img className='modal-picture' src={picture} alt={name}></img>
+                    <div className='github'>
+                        <a href={github} target='_blank' >GitHub Repository</a>
+                        <a href={deployed} target='_blank' >Deployed Application</a>
+                    </div>
                     <p className='modal-description'>{description}</p>
                     <button className='modal-button' type="button" onClick={onClose}>Close</button>
                 </div>
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -6,32 +6,44 @@ function Project() {
         {
             picture: require('../../assets/projects/blog.png'),
             name: 'Tech Blog App',
-            description: 'Description is up in here'
+            description: 'Description is up in here',
+            github:'https://github.com/samvrny/theTechBlog',
+            deployed:'https://fast-island-88973.herokuapp.com/'
         },
         {
             picture: require('../../assets/projects/jsquiz.png'),
             name: 'History Quiz',
-            description: 'Description is up in here'
+            description: 'Description is up in here',
+            github:'https://github.com/samvrny/javascript-basics-test',
+            deployed:'https://samvrny.github.io/javascript-basics-test/'
         },
         {
             picture: require('../../assets/projects/homebase.png'),
             name: 'Homebase App',
-            description: 'Description is up in here'
+            description: 'Description is up in here',
+            github:'https://github.com/AlexCourtney18/homebase',
+            deployed:'https://enigmatic-bastion-69025.herokuapp.com/'
         },
         {
             picture: require('../../assets/projects/notetaker.png'),
             name: 'Notetaker App',
-            description: 'Description is up in here'
+            description: 'Description is up in here',
+            github:'https://github.com/samvrny/notetakerapp',
+            deployed:'https://protected-mesa-86289.herokuapp.com/'
         },
         {
             picture: require('../../assets/projects/weatherapp.png'),
             name: 'Local Weather',
-            description: 'Description is up in here'
+            description: 'Description is up in here',
+            github:'https://github.com/samvrny/simple-weather-app',
+            deployed:'https://samvrny.github.io/simple-weather-app/'
         },
         {
             picture: require('../../assets/projects/runbuddy.png'),
             name: 'Run Buddy',
-            description: 'Description is up in here'
+            description: 'Description is up in here',
+            github:'https://www.google.com/',
+            deployed:'https://www.google.com/'
         }
     ])
 
