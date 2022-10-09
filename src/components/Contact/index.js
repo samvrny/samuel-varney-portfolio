139223356a0e34f62a92b092a1ffb6f5befd0db0
samvrny <samvrny@gmail.com>
2022-10-09 18:08:38 -0500

    got functionality for contact section. Style is MUCH needed.

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,6 +1,23 @@
 function Contact() {
-    return(
-        <div>Hello World! Contact me!</div>
+    return (
+        <section className='contact-holder'>
+            <h1>Contact Me</h1>
+            <form>
+                <div className='input-div'>
+                    <label htmlFor='name'>Name:</label>
+                    <input type='text' name='name' placeholder='Enter Name' />
+                </div>
+                <div className='input-div'>
+                    <label htmlFor='email'>Email:</label>
+                    <input type='email' name='email' placeholder='Example: john@email.com' />
+                </div>
+                <div className='message'>
+                    <label htmlFor='message'>Message:</label>
+                    <textarea name='message' placeholder='Enter your message here' />
+                </div>
+            </form>
+            <button type='submit'>Submit</button>
+        </section>
     )
 }
 
