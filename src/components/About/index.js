cae7f976cb164d2bebcd468ebb474ef036d9be5e
samvrny <samvrny@gmail.com>
2022-10-11 17:51:44 -0500

    finished updating the contact me section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,9 +9,9 @@ function About() {
                     Does anyone really know how to brand themselves or what to write in these things?
                     Toast is good, and so are bagels. There are a couple of great bagel places I remember
                     from when I lived in Boulder CO, and one of them specifially had a delicious cream
-                    cheese lox spead. Can't wait to go back and eat some again. ANY ways -- what else to talk about?
-                    Weather is weather outside. Looks like there some trees, and other plants. Colors are 
-                    just radiation, isn't that neat? Alright later guys, gotta go figure out hot to conditionally render
+                    cheese lox spread. Can't wait to go back and eat some again. ANY ways -- what else to talk about?
+                    Weather is weather outside. Looks like there some trees, and other plants. Color is
+                    just light radiation, isn't that neat? Alright later guys, gotta go figure out how to conditionally render
                     this whole 'Navigation' system.
                 </p>
             </div>
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,22 +1,57 @@
+import { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
+
 function Contact() {
+    const [errorMessage, setErrorMessage] = useState('');
+
+    function handleChange(event) {
+        event.preventDefault();
+
+        if(event.target.name === 'email') {
+            const validEmail = validateEmail(event.target.value);
+
+            if(!validEmail) {
+                setErrorMessage('Your email is invalid.')
+            } else {
+                setErrorMessage('');
+            }
+        } else {
+            if(!event.target.value.length) {
+                setErrorMessage(`${event.target.name} is required`)
+            } else {
+                setErrorMessage('');
+            }
+        }
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log(formState)
+    }
+
     return (
         <section className='contact-holder'>
             <h1>Contact Me</h1>
-            <form>
-                <div className='input-div'>
-                    <label htmlFor='name'>Name:</label>
-                    <input type='text' name='name' placeholder='Enter Name' />
+            <form className='contact-form' onSubmit={handleSubmit}>
+                <div className='form-div'>
+                    <label htmlFor='Name'>Name:</label>
+                    <input type='text' name='Name' placeholder='Enter Name' onBlur={handleChange} />
                 </div>
-                <div className='input-div'>
+                <div className='form-div'>
                     <label htmlFor='email'>Email:</label>
-                    <input type='email' name='email' placeholder='Example: john@email.com' />
+                    <input type='email' name='email' placeholder='Example: john@email.com' onBlur={handleChange}/>
                 </div>
-                <div className='message'>
-                    <label htmlFor='message'>Message:</label>
-                    <textarea name='message' placeholder='Enter your message here' />
+                <div className='form-div'>
+                    <label htmlFor='Message'>Message:</label>
+                    <textarea name='Message' placeholder='Enter your message here' rows='10' onBlur={handleChange}/>
                 </div>
+                {errorMessage && (
+                    <div>
+                        <p>{errorMessage}</p>
+                    </div>
+                )}
+                <button className='contact-btn' type='submit'>Submit</button>
             </form>
-            <button type='submit'>Submit</button>
         </section>
     )
 }
