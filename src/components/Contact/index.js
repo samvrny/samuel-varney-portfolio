f5ab9c99c72446c06f28f06930fc9352f421201f
samvrny <samvrny@gmail.com>
2022-10-23 15:39:19 -0500

    fixed minor error with class

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -6,7 +6,7 @@ function Contact() {
             <h1>Contact Me</h1>
             <div className='email-div'>
                 <p className='weight'>Contact me by email:</p>
-                <a class='email weight' href='mailto:samvrny@gmail.com'>samvrny@gmail</a>
+                <a className='email weight' href='mailto:samvrny@gmail.com'>samvrny@gmail</a>
             </div>
             <div>
                 <p className='weight'>
