1d522dbad49417eb7ba8266e7f404684a6579432
samvrny <samvrny@gmail.com>
2023-01-03 14:54:42 -0600

    changed about section text

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -33,16 +33,15 @@ function About() {
                 <div className='about-div'>
                     <img className='me second-sneak-hide' src={require('../../assets/samvarney.jpg')}></img>
                     <p className='about-me'> Hello! My name is Samuel Varney, and I am a fullstack web developer with an emphasis on front-end development.
-                        I recently earned a certificate in full stack web
-                        development from the University of Wisconsin through 2U with newly developed skills in JavaScript,
-                        HTML, CSS, React, SQL, NoSQL, Node, the MERN stack, and responsive web design.
-                        For the past 10 years I have worked across a few different industries and through a little trial and error, am happy to have found web development as a final career path
-                        because it aligns with my passions and future goals. I love solving puzzles, being mentally stimulated, and learning new things... that
-                        coupled with my goal of achieving financial freedom for my family makes me very excited to be entering into a career of web development!
-                        I am known as a hard worker with a strong work ethic, and I enjoy picking up new skills and applying to develop and debug web
-                        applications. With each project I hope to not only learn something new, but design apps
-                        that are user friendly and have an impactful user experience. I am very excited to leverage my skills as part of a quality-driven
-                        team to build better experiences on the web!
+                        For the past 10 years I have worked across a few different industries and through a little trial and error, am happy to have chosen web
+                        development as a permanent career path because it aligns with my passions and future goals. In November of 2022 I graduated from the
+                        University of Wisconsins’ Web Development bootcamp that focuses on Javascript and the MERN stack, and earned a certificate in full stack
+                        web development from the UW. During the course of the bootcamp I developed new skills in many technologies including JavaScript ES6+,
+                        HTML, CSS, React, SQL, NoSQL, Node, the MERN stack, and responsive web design. I love solving puzzles, being creative, and learning new
+                        things. I am known as a hard worker with a strong work ethic, and I enjoy picking up new coding skills and applying to develop and debug
+                        web applications. Each project I work on I take as an opportunity not only to learn something new, but design apps that are interesting,
+                        user friendly and have an impactful user experience. I am very excited to leverage my skills as part of a quality-driven team to build better
+                        experiences on the web!
                     </p>
                 </div>
             </div>
